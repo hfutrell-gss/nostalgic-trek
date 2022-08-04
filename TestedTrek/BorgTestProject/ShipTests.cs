644d99f6d4376a6dc78a1e0c8eea2df619d20c0a
bbeasley-gss <62615885+bbeasley-gss@users.noreply.github.com>
2022-08-04 12:31:07 -0500

    initial commit for ship

---
+++ TestedTrek/BorgTestProject/ShipTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImplementationProject;
+using System;
+
+namespace BorgTestProject
+{
+    [TestClass]
+    public class ShipTests
+    {
+        //20000
+
+
+        [TestMethod]
+        public void InitialRandomValue()
+        {
+            var ship = new Ship
+            {
+                SubsystemCount = 1
+            };
+            Assert.AreEqual(0, ship.GetRandomValue(ship.SubsystemCount));
+        }
+
+
+        [TestMethod]
+        public void GetRandomSystem()
+        {
+            var ship = new Ship
+            {
+                SubsystemCount = 1
+            };
+            Assert.IsNotNull(ship.GetRandomSystem());
+        }
+
+
+    }
+
+}
---
+++ TestedTrek/ImplementationProject/Ship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationProject
+{
+    public class Ship
+    {
+        public int SubsystemCount { get; set; }
+        public static Random generator = new Random();
+        private Subsystem[] subsystems = new Subsystem[] { new Subsystem() };
+
+        public Ship()
+        {
+        }
+
+        public int GetRandomValue(int maximum)
+        {
+            return generator.Next(maximum);
+        }
+
+        public Subsystem GetRandomSystem()
+        {
+            return subsystems[GetRandomValue(SubsystemCount)];
+        }
+    }
+}
