4a0344950d55c66c054eeab8030168b3d3e07197
bbeasley-gss <62615885+bbeasley-gss@users.noreply.github.com>
2022-08-04 13:36:42 -0500

    Repair, IsOperational and parameters on constructor for Subsystem

---
+++ TestedTrek/BorgTestProject/SubsystemTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImplementationProject;
+using System;
+
+namespace BorgTestProject
+{
+    [TestClass]
+    public class SubsystemTests
+    {
+
+        [TestMethod]
+        public void GetDamage()
+        {
+            var subsystem = new Subsystem();
+
+            subsystem.Damage = 100;
+
+            Assert.AreEqual(100, subsystem.Damage);
+        }
+
+
+        [TestMethod]
+        public void TakeDamage()
+        {
+            var subsystem = new Subsystem();
+
+            subsystem.TakeDamage(100);
+
+            Assert.AreEqual(100, subsystem.Damage);
+        }
+
+
+        [TestMethod]
+        public void IsOperationalTrue()
+        {
+            var subsystem = new Subsystem();
+
+            Assert.IsTrue(subsystem.IsOperational);
+        }
+
+
+        [TestMethod]
+        public void IsOperationalFalse()
+        {
+            var subsystem = new Subsystem();
+
+            subsystem.TakeDamage(100);
+
+            Assert.IsFalse(subsystem.IsOperational);
+        }
+
+        [DataRow("Phasers", 300)]
+        [TestMethod]
+        public void Repair(string subsystemName, int repairRate)
+        {
+            var subsystem = new Subsystem(subsystemName, repairRate);
+
+            subsystem.TakeDamage(300);
+
+            subsystem.Repair(1);
+
+            Assert.IsTrue(subsystem.IsOperational);
+        }
+
+
+    }
+
+}
--- TestedTrek/ImplementationProject/Subsystem.cs
+++ TestedTrek/ImplementationProject/Subsystem.cs
@@ -8,5 +8,26 @@ namespace ImplementationProject
 {
     public class Subsystem
     {
+        private string subsystemName;
+        private int repairRate;
+
+        public Subsystem(string subsystemName = "Unknown", int repairRate = 500)
+        {
+            this.subsystemName = subsystemName;
+            this.repairRate = repairRate;
+        }
+
+        public int Damage { get; set; }
+        public bool IsOperational { get { return Damage == 0; } }
+
+        public void TakeDamage(int amount)
+        {
+            Damage += amount;
+        }
+
+        public void Repair(int days)
+        {
+            Damage -= repairRate * days;
+        }
     }
 }
